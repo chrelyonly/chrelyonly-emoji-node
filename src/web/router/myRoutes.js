864f672331e08e0ad01bc9dbea6a63aeb3b7540e
chrelyonly <1172576293@qq.com>
2025-06-19 14:00:12 +0800

    1.整理代码

--- src/web/router/myRoutes.js
+++ src/web/router/myRoutes.js
@@ -3,7 +3,7 @@ const { textOnGifApi, uploadEmojiApi } = require('../controller/emojiController'
 
 const myRouter = express.Router();  // 创建路由实例
 
-myRouter.post('/emoji-app/emoji/uploadEmoji', uploadEmojiApi);
-myRouter.post('/emoji-app/emoji/textToGif', textOnGifApi);
+myRouter.post('/emoji/uploadEmoji', uploadEmojiApi);
+myRouter.post('/emoji/textToGif', textOnGifApi);
 
 module.exports = myRouter;
